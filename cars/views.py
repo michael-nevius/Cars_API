b7d8956509080bce0dde8fef6f441613838c1547
michael-nevius <michael.a.nevius@gmail.com>
2022-04-12 16:01:23 -0400

    Registered Car model with admin site + completed post functionality for car + updated rest framework decimal settings

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,19 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import CarSerializer
+from .models import Car
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def cars_list(request):
+    if request.method == 'GET':
+        cars = Car.objects.all()
+        serilizer = CarSerializer(cars, many=True)   
+        return Response(serilizer.data)
 
-
-    return Response('ok')
+    elif request.method == 'POST':
+        serilizer = CarSerializer(data=request.data)
+        serilizer.is_valid(raise_exception=True)
+        serilizer.save()
+        return Response(serilizer.data, status=status.HTTP_201_CREATED)
+    
